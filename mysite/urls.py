dc12b55e4e9d009f3c7cca502bdaa54ad5a14149
rafid-mirza <rafid.mirza@gmail.com>
2022-03-13 16:59:36 -0400

    added url

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('buddiesforstudies/', include('buddiesforstudies.urls')),
+    path('studiebuddies/', include('studiebuddies.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
