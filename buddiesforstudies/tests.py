d7bcc71af1f26305e8037b6d83eec95273ea1f68
iTechnoGeek <matthew.jsamuel@gmail.com>
2022-04-11 12:45:47 -0400

    added tests for toggling/untoggling

--- buddiesforstudies/tests.py
+++ buddiesforstudies/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model, authenticate
 from django.urls import reverse
-from .models import jsonData
+from .models import jsonData, toggled_classes
 
 
 class PracticeTests(TestCase):
@@ -68,3 +68,102 @@ class RemoveClassTests(TestCase):
         url = reverse('index')
         response = self.client.get(url)
         self.assertContains(response, 'You do not have any classes added')
+
+class ToggleClassTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='test', email='t@st.com')
+        self.user.save()
+        self.client.login(username='test', password='test')
+        data = jsonData(label="classes")
+        data.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_add_real_class(self):
+        title = 'CS 3240'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 4102'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 3102'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_toggle_real_class(self):
+        choice = ['CS 3240', 'CS 3102']
+        self.client.post(reverse('toggle'),{'choice':choice})
+        qs = toggled_classes.objects.all()
+        self.assertQuerysetEqual(list(qs),['<toggled_classes: CS 3240>', '<toggled_classes: CS 3102>'])
+
+
+
+class UntoggleClassTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='test', email='t@st.com')
+        self.user.save()
+        self.client.login(username='test', password='test')
+        data = jsonData(label="classes")
+        data.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_add_real_class(self):
+        title = 'CS 3240'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 4102'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 3102'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 9999'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_add_real_class(self):
+        title = 'CS 4414'
+        self.client.post(reverse('submit'), {'title':title})
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertContains(response, title)
+
+    def test_toggleuntoggle_real_class(self):
+        choice = ['CS 3240', 'CS 3102', 'CS 4414']
+        self.client.post(reverse('toggle'),{'choice':choice})
+        qs = toggled_classes.objects.all()
+        self.assertQuerysetEqual(list(qs),['<toggled_classes: CS 3240>', '<toggled_classes: CS 3102>', '<toggled_classes: CS 4414>'])
+
+        choice = ['CS 3102', 'CS 4414']
+        self.client.post(reverse('untoggle'),{'choice':choice})
+        qs1 = toggled_classes.objects.all()
+        self.assertQuerysetEqual(list(qs1),['<toggled_classes: CS 3240>'])
