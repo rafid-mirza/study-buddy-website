563470dc1abd6c5fe2aecfd0538f04da5e727393
M8Name <macteefey@gmail.com>
2022-04-18 14:24:13 -0400

    Forgot to add tests
    The tests work right now

--- buddiesforstudies/tests.py
+++ buddiesforstudies/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model, authenticate
 from django.urls import reverse
-from .models import jsonData, toggled_classes, classes
+from .models import jsonData, toggled_classes, classes, Room
 
 
 class PracticeTests(TestCase):
@@ -183,3 +183,36 @@ class UntoggleClassTests(TestCase):
         choice6 = []
         response4 = self.client.post(reverse('untoggle'), {'choice': choice6})
         self.assertContains(response4, 'You did not select a class.')
+
+class HomePageTest(TestCase):
+    def test_all_rooms_are_rendered_in_homepage(self):
+        Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room'
+        )
+        Room.objects.create(
+            name='room 2',
+            slug='room-2',
+            description='This is the 2nd room'
+        )
+
+        response = self.client.get('/buddiesforstudies/rooms/')
+
+        self.assertContains(response, 'room 1')
+        self.assertContains(response, 'room 2')
+
+
+class RoomDetailTest(TestCase):
+
+    def test_room_details_are_present_in_room_page(self):
+        room_1 = Room.objects.create(
+            name='room X',
+            slug='room-x',
+            description='This is the X-room'
+        )
+
+        response = self.client.get('/buddiesforstudies/rooms/{}/'.format(room_1.slug))
+
+        self.assertContains(response, room_1.name)
+        self.assertContains(response, room_1.description)
