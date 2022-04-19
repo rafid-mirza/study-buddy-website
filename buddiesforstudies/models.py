9292d6e9acc5e466c680f379998549211e416502
rafid-mirza <rafid.mirza@gmail.com>
2022-04-18 22:18:01 -0400

    added support for listing upcoming sessions

--- buddiesforstudies/models.py
+++ buddiesforstudies/models.py
@@ -10,8 +10,9 @@ class Location(models.Model):
     address = AddressAutoHiddenField(default = "")
     def __str__(self):
         return self.address
-    user_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_2', null = TRUE)
     user_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_1', null = TRUE)
+    user_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_2', null = TRUE)
+    
     
 
 
