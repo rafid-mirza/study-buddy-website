763e842eda1253cc7fabd2b9280e47c1bd1f117e
M8Name <macteefey@gmail.com>
2022-05-03 03:05:41 -0400

    Forgot that models have to have a max length

--- buddiesforstudies/models.py
+++ buddiesforstudies/models.py
@@ -73,7 +73,7 @@ class participant(models.Model):
 
 
 class conversation(models.Model):
-    friendly_name = models.CharField()
+    friendly_name = models.CharField(max_length=128)
     chat_id = models.CharField(max_length=128)
     participants = models.ManyToManyField(participant, verbose_name="List of Chat Members")
 
