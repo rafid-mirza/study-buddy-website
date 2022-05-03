07240a01f08a896dd78d4c6ca82c0a2e7ae6a9df
M8Name <macteefey@gmail.com>
2022-05-03 11:37:15 -0400

    Update tests.py

--- buddiesforstudies/tests.py
+++ buddiesforstudies/tests.py
@@ -184,22 +184,5 @@ class UntoggleClassTests(TestCase):
         response4 = self.client.post(reverse('untoggle'), {'choice': choice6})
         self.assertContains(response4, 'You did not select a class.')
 
-class HomePageTest(TestCase):
-    def test_all_rooms_are_rendered_in_homepage(self):
-        Room.objects.create(
-            name='room 1',
-            slug='room-1',
-            description='This is the 1st room'
-        )
-        Room.objects.create(
-            name='room 2',
-            slug='room-2',
-            description='This is the 2nd room'
-        )
-
-        response = self.client.get('/buddiesforstudies/rooms/')
-
-        self.assertContains(response, 'room 1')
-        self.assertContains(response, 'room 2')
 
 
